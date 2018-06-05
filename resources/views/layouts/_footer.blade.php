5113e65d3b45df205b1c9273941b4aefdea6009d
litmonw <litmonw@163.com>
2018-06-05 08:57:46 +0000

    feature: Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,14 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <a href="https://github.com/litmonw/laravel-start-demo">
+                Github Links
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
