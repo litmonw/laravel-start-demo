8488923ff854dd1a09ccaed3d90151ef82a94c27
litmonw <litmonw@163.com>
2018-07-03 21:57:48 +0800

    Feature: 错误提交

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
     public function create() {
         return view('users.create');
+    }
+
+    // Laravel 会自动解析定义在控制器方法（变量名匹配路由片段）中的 Eloquent 模型类型声明
+    public function show(User $user) {
+        return view('users.show', compact('user'));
     }
 }
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.default')
+@section('title', $user->name)
+@section('content')
+    <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+            <div class="col-md-12">
+                <div class="col-md-offset-2 col-md-8">
+                    <section class="user_info">
+                        {{--这里的 ['user' => $user] 不用添加，仅当你控制器传来的变量就是$user，然后@include里的也是要$user，那是可以不传--}}
+                        {{--但是如果你控制器没传，就需要['user' => Auth::user()]。--}}
+                        {{--或者@include里的变量是$u，或者其他，那就要['u' => $user]这样传一下--}}
+                        @include('shared._user_info', ['user' => $user])
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{$user->name}} - {{ $user->email }}
+@stop
