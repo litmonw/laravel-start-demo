17892e61e45a2605f3b4e3ec576142de8b25ad11
litmonw <litmonw@163.com>
2018-07-17 23:54:15 +0800

    no message

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,9 +12,10 @@
     @include('layouts._header')
 
     <div class="container">
-        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
     </div>
+
+    <script src="/js/app.js"></script>
 </body>
 </html>
