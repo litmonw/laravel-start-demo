347963e5776d6dab562accba62bf6331d3ef9eb7
litmonw <litmonw@163.com>
2018-07-17 23:48:12 +0800

    no message

--- resources/views/shared/_errors.blade.php
+++ resources/views/shared/_errors.blade.php
@@ -1,9 +1,7 @@
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dsyal
+ * Date: 2018/7/5
+ * Time: 下午9:40
+ */
